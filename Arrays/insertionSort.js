0e1e66cf74efd4042a1d4421ca7b9e4c7e9937ff
Himanshu salhotra <30443624+salhotra8@users.noreply.github.com>
2025-06-06 19:33:23 +0530

    Update insertionSort.js

--- Arrays/insertionSort.js
+++ Arrays/insertionSort.js
@@ -1,13 +1,16 @@
+/**
+* @param {any[]} arr
+*/
 function insertionSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i; j > -1; j--) {
-            if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
-            }
-            console.log(arr)
-        }
-    }
-    return arr
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i],
+    j;
+    for (j = i - 1; j > -1 && arr[j] > current; j--) {
+      arr[j+1] = arr[j]
+  }
+  arr[j + 1] = current; 
+  }
+return arr
 }
 
 const result = insertionSort([1, 5, 34, 23, 8, 4, 45, 10]);
