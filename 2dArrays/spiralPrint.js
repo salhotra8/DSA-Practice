b7966192fb31006e3f5082ed155ca5be91b06b35
salhotra8 <salhotra8@gmail.com>
2025-07-01 19:01:56 +0530

    improvements

--- 2dArrays/spiralPrint.js
+++ 2dArrays/spiralPrint.js
@@ -1,37 +1,46 @@
 // Main
 function spiralPrint(M) {
-    var topRow = 0,
-        leftCol = 0,
-        btmRow = M.length - 1,
-        rightCol = M[0].length - 1;
+  let leftCol = 0,
+    rightCol = M[0].length - 1,
+    topRow = 0,
+    bottomRow = M.length - 1;
 
-    while (topRow < btmRow && leftCol < rightCol) {
-        for (var col = 0; col <= rightCol; col++) {
-            console.log(M[topRow][col]);
-        }
-        topRow++;
-        for (var row = topRow; row <= btmRow; row++) {
-            console.log(M[row][rightCol]);
-        }
-        rightCol--;
-        if (topRow <= btmRow) {
-            for (var col = rightCol; col >= leftCol; col--) {
-                console.log(M[btmRow][col]);
-            }
-            btmRow--;
-        }
-        if (leftCol <= rightCol) {
-            for (var row = btmRow; row > topRow; row--) {
-                console.log(M[row][leftCol]);
-            }
-            leftCol++;
-        }
+  while (leftCol <= rightCol && topRow <= bottomRow) {
+    // Print top row (left to right)
+    for (let i = leftCol; i <= rightCol; i++) {
+      console.log(M[topRow][i]);
     }
+    topRow++;
+
+    // Break if rows boundaries have crossed
+    if (topRow > bottomRow) break;
+
+    // Print right column (top to bottom)
+    for (let i = topRow; i <= bottomRow; i++) {
+      console.log(M[i][rightCol]);
+    }
+    rightCol--;
+
+    // Break if column boundaries have crossed
+    if (leftCol > rightCol) break;
+
+    // Print bottom row (right to left)
+    for (let i = rightCol; i >= leftCol; i--) {
+      console.log(M[bottomRow][i]);
+    }
+    bottomRow--;
+
+    // Print left column (bottom to top)
+    for (let i = bottomRow; i >= topRow; i--) {
+      console.log(M[i][leftCol]);
+    }
+    leftCol++;
+  }
 }
 var M = [
-    [1, 2, 3, 4, 5],
-    [6, 7, 8, 9, 10],
-    [11, 12, 13, 14, 15],
-    [16, 17, 18, 19, 20]
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
 ];
 spiralPrint(M);
---
+++ Linked List/reverseSinglyLinkedList.js
@@ -0,0 +1,16 @@
+function reverseSinglyLinkedList(head) {
+  let curr = head;
+  let prev = null;
+
+  while (curr) {
+    let temp = curr.next;
+    curr.next = prev;
+
+    prev = curr;
+    
+    curr = temp;
+  }
+  return prev;
+}
+
+
